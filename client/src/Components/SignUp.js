6d3aecacb46e96bbe0795a8cfd2f1b0ccc2992e4
Eric Lee <Raidenlkl@gmail.com>
2018-12-22 23:21:19 +1000

    basic signup page

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -3,9 +3,42 @@ import React, {Component} from 'react';
 class SignUp extends Component{
     render(){
         return(
-            <div>
-                <h1>SignUp</h1>
-            </div>
+        <div className="ui container">
+            <form className="ui form">
+
+                <div className="field">
+                    <label>First Name</label>
+                    <input type="text" name="firstname" placeholder="First Name"/>
+                </div>
+
+                <div className="field">
+                    <label>Last Name</label>
+                    <input type="text" name="lastname" placeholder="Last Name" />
+                </div>
+
+                <div className="field">
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="Email" />
+                </div> 
+
+                <div class="field">
+                    <select>
+                    <option value="">Gender</option>
+                    <option value="1">Male</option>
+                    <option value="0">Female</option>
+                    </select>
+                </div>
+
+                <div className="field">
+                    <div className="ui checkbox">
+                        <input type="checkbox"/>
+                        <label>I agree to the Terms and Conditions</label>
+                    </div>
+                </div>
+
+                <button className="ui button" type="submit">Submit</button>
+            </form>
+        </div>
         )
     }
 }
