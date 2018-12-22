958b279e48b6b2206d2f2d4678ac4a1f29a5de62
Eric Lee <Raidenlkl@gmail.com>
2018-12-23 03:32:24 +1000

    Proxy setup successfully

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -1,8 +1,13 @@
 import {REGISTER_MEMBER} from './types'
+import axios from 'axios';
 
-export const signUp = (member) => {
-    return {
+export const signUp = (member) => async dispatch => {
+
+    const res = await axios.post('/api/student', member);
+    console.log(res);
+
+    dispatch({
         type: REGISTER_MEMBER,
         payload: member
-    }
+    })
 }
