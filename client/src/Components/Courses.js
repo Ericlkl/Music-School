df806f5d488fe530adad2f91e499c95ef0cbca3d
Eric Lee <Raidenlkl@gmail.com>
2018-12-23 18:32:03 +1000

    Teacher card and course card

--- client/src/Components/Courses.js
+++ client/src/Components/Courses.js
@@ -3,17 +3,57 @@ import NavBar from './NavBar';
 import Footer from './Footer';
 import bg_img from '../assets/img/course_bg.jpg'
 
+const CourseCard = (props) => {
+    return (
+        <div className="card">
+            <div className="image">
+                <img src="/images/avatar2/large/matthew.png"/>
+            </div>
+            <div className="content">
+                <div className="header">Teacher Name</div>
+
+                <div className="meta">
+                    <p>Bachelor</p>
+                </div>
+
+                <div className="description">
+                    This is the description box.
+                </div>
+
+            </div>
+            
+            <div className="extra content">
+                <span className="right floated">
+                    1 Years+ Experiences
+                </span>
+                <span>
+                    <i className="user icon"></i>
+                    75 Students
+                </span>
+            </div>
+        </div>
+    )
+}
+
 class Courses extends Component{
     render(){
         return(
             <div>
-            <NavBar page="courses"
+                <NavBar page="courses"
                     backgroundImg={bg_img}
                     title="Courses"
                     subtitle="Make the change Today!"/>
-                <h1>
-                    Courses
-                </h1>
+
+                    <div style={{
+                        margin: "50px"
+                    }} className="ui container">
+                        <div class="ui link cards">
+                        <CourseCard/>
+                        <CourseCard/>
+                        <CourseCard/>
+                        <CourseCard/>
+                        </div>
+                    </div>
                 <Footer/>
             </div>
         )
--- client/src/Components/Teachers.js
+++ client/src/Components/Teachers.js
@@ -3,6 +3,37 @@ import NavBar from './NavBar';
 import Footer from './Footer';
 import bg_img from '../assets/img/teachers_bg.jpg'
 
+const TeacherCard = (props) => {
+    return (
+        <div className="card">
+            <div className="image">
+                <img src="/images/avatar2/large/matthew.png"/>
+            </div>
+            <div className="content">
+                <div className="header">Teacher Name</div>
+
+                <div className="meta">
+                    <p>Bachelor</p>
+                </div>
+
+                <div className="description">
+                    This is the description box.
+                </div>
+
+            </div>
+            
+            <div className="extra content">
+                <span className="right floated">
+                    1 Years+ Experiences
+                </span>
+                <span>
+                    <i className="user icon"></i>
+                    75 Students
+                </span>
+            </div>
+        </div>
+    )
+}
 
 class Teachers extends Component{
     render(){
@@ -13,7 +44,17 @@ class Teachers extends Component{
                     title="Teachers"
                     subtitle="Professional Teachers in Australia"/>
                 
-                <h1>Teachers</h1>
+                    <div style={{
+                        margin: "50px"
+                    }} className="ui container">
+                        <div class="ui link cards">
+                        <TeacherCard/>
+                        <TeacherCard/>
+                        <TeacherCard/>
+                        <TeacherCard/>
+                        </div>
+                    </div>
+
                 <Footer/>
             </div>
         )
