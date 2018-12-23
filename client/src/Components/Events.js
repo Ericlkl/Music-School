0bc4ff5aaff44658eb4f5e81aa3d28e073443c20
Eric Lee <Raidenlkl@gmail.com>
2018-12-23 21:45:50 +1000

    event card render

--- client/src/Components/Events.js
+++ client/src/Components/Events.js
@@ -6,6 +6,43 @@ import { fetchEvents} from '../Actions/index'
 
 import bg_img from '../assets/img/event_bg.jpg'
 
+const EventCard = (props) => {
+    const {eventName, imageURI, date, place, description, tag, company} = props.event;
+    var newDate = new Date(date.toString());
+
+    return (
+        <div className="card">
+            <div className="image">
+                <img src={imageURI} alt="event-img"/>
+            </div>
+            <div className="content">
+                <div className="header">{eventName}</div>
+
+                <div className="meta">
+                    <p>{tag.map(skill => skill)}</p>
+                    <p> <i className="calendar alternate outline icon"></i>
+                    { newDate.toLocaleTimeString() + " " + newDate.toLocaleDateString() }</p>
+                </div>
+
+                <div className="description">
+                    {description}
+                </div>
+                
+            </div>
+            
+            <div className="extra content">
+                <span className="right floated">
+                    By {company}
+                </span>
+                <span>
+                    <i className="map marker icon"></i>
+                    {place}
+                </span>
+            </div>
+        </div>
+    )
+}
+
 class Events extends Component{
     componentDidMount(){
         this.props.fetchEvents();
@@ -16,13 +53,16 @@ class Events extends Component{
 
         return(
             <div>
-            <NavBar page="events"
-                    backgroundImg={bg_img}
-                    title="Events"
-                    subtitle="Jumb into Biggest Music Event in Aus"/>
-                <h1>
-                    Events
-                </h1>
+                <NavBar page="events"
+                        backgroundImg={bg_img}
+                        title="Events"
+                        subtitle="Jumb into Biggest Music Event in Aus"/>
+
+                <div style={{ margin: "50px"}} className="ui container">
+                    <div className="ui link cards">
+                    {this.props.events.map(event => <EventCard event={event} /> )}
+                    </div>
+                </div>
                 <Footer/>
             </div>
         )
