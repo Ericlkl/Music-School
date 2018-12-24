c37c060498e98f6518d88b124231746c047661b0
Eric Lee <Raidenlkl@gmail.com>
2018-12-24 23:00:45 +1000

    Description Box in About US

--- client/src/Components/AboutUs.js
+++ client/src/Components/AboutUs.js
@@ -3,6 +3,38 @@ import NavBar from './NavBar';
 import Footer from './Footer';
 import bg_img from '../assets/img/aboutus_bg.jpg'
 
+const Description = () => {
+    return (
+        <div className="ui vertical stripe segment">
+            <div className="ui middle aligned stackable grid container">
+
+            <div style={{
+                padding:'30px'
+            }} className="row">
+                <div className="center aligned column">
+                    <h1>What We do...</h1>
+                </div>
+            </div>
+
+            <div style={{
+                marginTop: '30px',
+                marginBottom: '30px'
+            }} className="row">
+                <div className="eight wide column">
+                <h3 className="ui header">The best learning experience for music</h3>
+                <p>Experience music lessons at their best. Guitar lessons, piano lessons, singing lessons, drum lessons, it's all here at Forte School of Music. </p>
+                <h3 className="ui header">It is never be late to start</h3>
+                <p> We teach baby music, music for kids, music for adults.</p>
+                </div>
+                <div className="seven wide right floated column">
+                <img src={bg_img} className="ui large bordered rounded image"/>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
 class AboutUs extends Component{
     render(){
         return(
@@ -11,9 +43,7 @@ class AboutUs extends Component{
                     backgroundImg={bg_img}
                     title="About US"
                     subtitle="Professional Music Insititute in Australia"/>
-                <h1>
-                    AboutUs
-                </h1>
+                <Description/>
                 <Footer/>
             </div>
         )
