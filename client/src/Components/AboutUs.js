dbc6ae695ea524962933411ec4b92fd32351c6a1
Eric Lee <Raidenlkl@gmail.com>
2019-02-10 16:34:54 +0800

    About us page fixed

--- client/src/Components/AboutUs.js
+++ client/src/Components/AboutUs.js
@@ -1,83 +1,33 @@
 import React, {Component} from 'react';
 import NavBar from './NavBar';
+import InfoSection from './InfoSection';
 import Hero from './Hero';
 import Footer from './Footer';
 import bg_img from '../assets/img/aboutus_bg.jpg'
-
-const Description = () => {
-    return (
-        <div className="ui vertical stripe segment">
-            <div className="ui middle aligned stackable grid container">
-
-            <div style={{
-                padding:'30px'
-            }} className="row">
-                <div className="center aligned column">
-                    <h1>Our History</h1>
-                </div>
-            </div>
-
-            <div style={{
-                marginTop: '30px',
-                marginBottom: '30px'
-            }} className="row">
-            <div className="seven wide right floated column">
-            <img src={bg_img} className="ui large bordered rounded image"/>
-            </div>
-            
-                <div className="eight wide column">
-                <h3 className="ui header">The best learning experience for music</h3>
-                <p>Experience music lessons at their best. Guitar lessons, piano lessons, singing lessons, drum lessons, it's all here at Forte School of Music. </p>
-                <h3 className="ui header">It is never be late to start</h3>
-                <p> We teach baby music, music for kids, music for adults.</p>
-                </div>
-            </div>
-            </div>
-        </div>
-    )
-}
-
-const Contact = () => {
-  
-    return (
-        <div style={{backgroundColor: "#f4f4f4"}} className="ui vertical stripe segment">
-            <div className="ui middle aligned stackable grid container">
-            <div style={{
-                padding:'30px'
-            }} className="row">
-                <div className="center aligned column">
-                    <h1>Contact US</h1>
-                </div>
-            </div>
-            <div style={{
-                marginTop: '30px',
-                marginBottom: '30px'
-            }} className="row">
-                <div className="eight wide column">
-                <h3 className="ui header">The best learning experience for music</h3>
-                <p>Experience music lessons at their best. Guitar lessons, piano lessons, singing lessons, drum lessons, it's all here at Forte School of Music. </p>
-                <h3 className="ui header">It is never be late to start</h3>
-                <p> We teach baby music, music for kids, music for adults.</p>
-                </div>
-                <div className="seven wide right floated column">
-                    <h1>Place map later</h1>
-                </div>
-            </div>
-            </div>
-        </div>
-    )   
-}
 
 class AboutUs extends Component{
     render(){
         return(
             <div>
-            <NavBar pages="aboutus"/>
-            <Hero bg_img={bg_img}
-                title="About US"
-                subtitle="Professional Music Insititute in Australia"/>
-                <Description/>
-                <Contact/>
+                <NavBar pages="aboutus"/>
+                <Hero bg_img={bg_img}
+                    title="About US"
+                    subtitle="Professional Music Insititute in Australia"/>
+
+                <InfoSection title="Our History" imgPosition="left" img={bg_img} >
+                    <h2>The best learning experience for music</h2>
+                    <p>Experience music lessons at their best. Guitar lessons, piano lessons, singing lessons, drum lessons, it's all here at Forte School of Music. </p>
+                    <h2>It is never be late to start</h2>
+                    <p> We teach baby music, music for kids, music for adults.</p>
+                </InfoSection>
+
+                <InfoSection title="Contact US" img={bg_img} bg_color={{backgroundColor: 'rgb(244, 244, 244)'}} >
+                    <h2>The best learning experience for music</h2>
+                    <p>Experience music lessons at their best. Guitar lessons, piano lessons, singing lessons, drum lessons, it's all here at Forte School of Music. </p>
+                    <h2>It is never be late to start</h2>
+                    <p> We teach baby music, music for kids, music for adults.</p>
+                </InfoSection>
+                
                 <Footer/>
             </div>
         )
