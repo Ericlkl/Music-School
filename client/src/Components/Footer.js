6937f7907e1f6688384556853aa17482059c3e7e
Eric Lee <Raidenlkl@gmail.com>
2019-02-10 13:52:45 +0800

    new footer scss version

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -4,35 +4,35 @@ import {Link} from 'react-router-dom';
 class Footer extends Component{
     render(){
         return(
-            <div style={{
-                padding: "50px",
-                marginTop: "100px"
-            }} className="ui inverted vertical footer segment">
-            <div className="ui container">
-              <div className="ui stackable inverted divided equal height stackable grid">
-                <div className="three wide column">
-                  <h4 className="ui inverted header">About</h4>
-                  <div className="ui inverted link list">
-                  <Link to="/courses" className="item">Courses</Link>
-                  <Link to="/events" className="item">Events</Link>
-                  <Link to="/teachers" className="item">Teachers</Link>
-                  <Link to="/aboutus" className="item">AboutUs</Link>
-                  </div>
-                </div>
-                <div className="three wide column">
-                  <h4 className="ui inverted header">Services</h4>
-                  <div className="ui inverted link list">
-                    <Link to="/" className="item">Banana Pre-Order</Link>
-                    <Link to="/" className="item">DNA FAQ</Link>
-                    <Link to="/" className="item">How To Access</Link>
-                    <Link to="/" className="item">Favorite X-Men</Link>
-                  </div>
-                </div>
-                <div className="seven wide column">
-                  <h4 className="ui inverted header">Developed By KA LONG LEE (Eric)</h4>
-                  <p>It is a profolio project for building a fullstack web application for music school. The school is not exist in the real world.</p>
-                </div>
+          <div className="footer">
+            <div className="container">
+              <div className="page-links">
+                  <h1>Pages</h1>
+                  <Link to="/courses" className="link">Courses</Link>
+                  <Link to="/events" className="link">Events</Link>
+                  <Link to="/teachers" className="link">Teachers</Link>
+                  <Link to="/aboutus" className="link">AboutUs</Link>  
               </div>
+
+              <div className="page-links">
+                <h1>Admin Portal</h1>
+                <Link to="/admin/register" className="link">Register Admin</Link>
+                <Link to="/admin/login" className="link">Login Admin</Link>
+                <Link to="/admin/courses" className="link">CMS</Link>
+              </div>
+
+              <div className="social-medias">
+                <i class="fab fa-facebook-square fa-3x"></i>
+                <i class="fab fa-instagram fa-3x"></i>
+                <i class="fab fa-twitter-square fa-3x"></i>
+                <i class="fab fa-google-plus-square fa-3x"></i>
+              </div>
+
+              <div className="copyright">
+                <h1>Developed By KA LONG LEE (Eric)</h1>
+                <h3>It is a profolio project for building a fullstack web application for music school. The school is not exist in the real world.</h3>
+              </div>
+
             </div>
           </div>
         )
