4eb2c59060293218ccf36407de6afd5d950e9eae
Eric Lee <Raidenlkl@gmail.com>
2019-02-15 07:37:32 +0800

    login page ui complete

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -4,26 +4,37 @@ class Login extends Component{
     render(){
         return(
           <div className="login">
-            <div className="login-form">
-              <h1 className="company-name">Pineland Music School</h1>
+            <form className="login-form py-5">
+              <h1 className="udl-heading company-name">Pineland Music School</h1>
 
-              <label htmlFor="username" >Email or Username</label>
-              <input type="text" name="username"/>
+              <div className="field ui input">  
+                <label htmlFor="username" >Email or Username </label>
+                <input type="text" name="username"/>
+              </div>
 
-              <label htmlFor="password" >Password</label>
-              <input type="text" name="password"/>
+              <div className="field ui input">
+                <label htmlFor="password" >Password </label>
+                <input type="text" name="password"/>
+              </div>
 
               <button className="ui button">Sign In</button>
               <a href="#">forget password?</a>
 
               <div className="social-login">
-                <button>Google</button>
-                <button>Facebook</button>
+                <button class="ui facebook button">
+                  <i class="facebook icon"></i>
+                  Facebook
+                </button>
+
+                <button class="ui google plus button">
+                  <i class="google icon"></i>
+                  Google
+                </button>
               </div>
 
-              <button>Create Account</button>
+              <button className="ui button primary">Create Account</button>
 
-            </div>
+            </form>
 
             <div className="login-img">
               <div className="container">
