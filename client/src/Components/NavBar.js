ca87139f2052d0e04425e73443af1b927321d652
Eric Lee <Raidenlkl@gmail.com>
2019-02-15 17:00:05 +0800

    hamburger menu completed

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -7,7 +7,8 @@ class NavBar extends Component{
     state= {
       width: 0,
       transformNavbar : false,
-      navbarOrientation: "vertical"
+      navbarOrientation: "vertical",
+      appendMenu: false
     }
     
     componentDidMount = () => {
@@ -28,15 +29,17 @@ class NavBar extends Component{
       });
 
       if (window.innerWidth > 854){
-        this.setState({navbarOrientation: "horizontial"})
+        this.setState({navbarOrientation: "horizontial", appendMenu: true })
       } else {
-        this.setState({navbarOrientation: "vertical", transformNavbar: true})
+        this.setState({navbarOrientation: "vertical", appendMenu: false , transformNavbar: true})
       }
+    }
+
+    hamburgerMenuClicked = () => {
+      this.setState({appendMenu : !this.state.appendMenu});
     }
     
     handleScroll = (event) => {
-      console.log('the scroll things', event.path[1].scrollY);
-      console.log('vh width', this.state.width);
       if(event.path[1].scrollY > 450){
         this.setState({ transformNavbar: true })
       } else {
@@ -46,21 +49,36 @@ class NavBar extends Component{
 
     render(){
         return (
-          <div id={this.state.transformNavbar === true ? "transfromNavbar" : "normalNavbar"} className="navbar">
+          <div id={this.state.transformNavbar === true || 
+            this.state.navbarOrientation === "vertical" ? "transfromNavbar" : "normalNavbar"} className="navbar">
             <div className="container">
+
               <div className={this.state.navbarOrientation}>
-                <div className="page-links">
+                <div className="hamburger-menu" 
+                    onClick={this.hamburgerMenuClicked}
+                style={ this.state.navbarOrientation === "vertical" ? 
+                  { display: 'flex' } : { display : 'none' }
+                }>
+                  <div className="line"/>
+                  <div className="line"/>
+                  <div className="line"/>
+                </div>
+
+                <div style={ this.state.appendMenu === false ? {display: 'none'} : {} } 
+                    className="page-links">
                   <Link className={this.props.pages === "home" ? "active route" : "route"} to="/">Home</Link>
                   <Link className={this.props.pages === "courses" ? "active route" : "route"} to="/courses">Course</Link>
                   <Link className={this.props.pages === "events" ? "active route" : "route"} to="/events">Events</Link>
                   <Link className={this.props.pages === "teachers" ? "active route" : "route"} to="/teachers">Teachers</Link>
                   <Link className={this.props.pages === "aboutus" ? "active route" : "route"} to="/aboutus">About Us</Link>
                 </div>
-                <div className="members-sections">
+                <div style={ this.state.appendMenu === false ? {display: 'none'} : {} }
+                 className="members-sections">
                   <Link className="ui large inverted button" to="/signup">Sign Up</Link>
                   <Link className="ui large inverted button" to="/login">Login</Link>
                 </div>
               </div>
+
             </div>
           </div>
         )
